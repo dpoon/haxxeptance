4767737ca250bf12042fed541eeb5e68a0975c31
Dara Poon <github@dwpoon.com>
2020-05-01 01:26:22 -0700

    Initial Commit

---
+++ Haxxeptance/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Haxxeptance
+//
+//  Created by Dara Poon on 2020-05-01.
+//  Copyright © 2020 Dara Poon. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HaxxeptanceTests/HaxxeptanceTests.swift
@@ -0,0 +1,34 @@
+//
+//  HaxxeptanceTests.swift
+//  HaxxeptanceTests
+//
+//  Created by Dara Poon on 2020-05-01.
+//  Copyright © 2020 Dara Poon. All rights reserved.
+//
+
+import XCTest
+@testable import Haxxeptance
+
+class HaxxeptanceTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
