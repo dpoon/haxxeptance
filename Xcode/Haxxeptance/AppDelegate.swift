f2f6f16e021a25de952a70f9943c0f3932343a1b
Dara Poon <github@dwpoon.com>
2020-05-01 19:07:15 -0700

    GNU GPL 2.0 or later license

--- Xcode/Haxxeptance/AppDelegate.swift
+++ Xcode/Haxxeptance/AppDelegate.swift
@@ -2,27 +2,51 @@
 //  AppDelegate.swift
 //  Haxxeptance
 //
-//  Created by Dara Poon on 2020-05-01.
-//  Copyright © 2020 Dara Poon. All rights reserved.
+//  Copyright © 2020 Dara Poon.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License along
+//  with this program; if not, write to the Free Software Foundation, Inc.,
+//  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 //
 
 import Cocoa
 
-//@NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    var appVersion: String {
+        return ["CFBundleName", "CFBundleShortVersionString", "NSHumanReadableCopyright"].map {
+            Bundle.main.object(forInfoDictionaryKey: $0) as! String
+        }.joined(separator: " ")
+    }
+    
+    let licenseSynopsis = """
+    This program comes with ABSOLUTELY NO WARRANTY.  This is free software,
+    and you are welcome to redistribute it under the terms of the GNU
+    General Public License version 2 or later.
+    """
+    
     let regex:NSRegularExpression = try! NSRegularExpression(
         pattern: "^hxxp(s?):", options: NSRegularExpression.Options.caseInsensitive
     )
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        print(appVersion)
+        print()
+        print(licenseSynopsis)
+        print()
         print("Installed handler for hxxp: and hxxps: URL schemes.")
         NSApplication.shared.terminate(self)
     }
-
-    //func applicationWillTerminate(_ aNotification: Notification) {
-    //    debugPrint("terminating")
-    //}
 
     func application(_ application: NSApplication, open urls: [URL]) {
         urls.forEach { (origUrl) in
